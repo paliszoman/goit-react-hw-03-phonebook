4511f35377f477599a916c9c4098908a1abec32d
paliszoman <kamilkrolik@hotmail.com>
2022-11-29 15:51:42 +0100

    fine

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -21,6 +21,20 @@ export class App extends Component {
     
   }
 
+  componentDidMount() {
+        if (this.state.contacts.length<=0) {
+      this.setState({contacts: JSON.parse(localStorage.getItem('phonebook'))})
+  }
+  }
+
+  componentDidUpdate() {
+  // Typowy sposób użycia (nie zapomnij porównać właściwości):
+  if (this.state !== localStorage.getItem('phonebook')) {
+    localStorage.setItem('phonebook', JSON.stringify(this.state.contacts))
+  }
+
+}
+
   //function for searching contacts by single letter
   searchForContacts(search) {
     const contacts = this.state.contacts;
@@ -33,6 +47,7 @@ export class App extends Component {
     }
     )
   }
+
 
 //function looking for contact if exist make alert
   searchForExistingContacts(contactFromForm) {
@@ -54,12 +69,7 @@ export class App extends Component {
   }
 
   state = {
-  contacts: [
-    {id: 'id-1', name: 'Rosie Simpson', number: '459-12-56'},
-    {id: 'id-2', name: 'Hermione Kline', number: '443-89-12'},
-    {id: 'id-3', name: 'Eden Clements', number: '645-17-79'},
-    {id: 'id-4', name: 'Annie Copeland', number: '227-91-26'},
-  ],
+  contacts: [  ],
   filter:[]
   }
 
