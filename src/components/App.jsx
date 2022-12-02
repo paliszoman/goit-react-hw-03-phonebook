a122a3916ef7ce0959817e0f2fc46af7112afdb1
paliszoman <kamilkrolik@hotmail.com>
2022-12-03 00:20:39 +0100

    app

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -71,7 +71,12 @@ export class App extends Component {
   }
 
   state = {
-    contacts: [],
+    contacts: [
+      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+    ],
     filter: [],
   };
 
@@ -82,18 +87,14 @@ export class App extends Component {
         <Form onSubmit={value => this.searchForExistingContacts(value)}></Form>
         <h2>Contacts</h2>
         <Filter onChange={search => this.searchForContacts(search)}></Filter>
-        {this.state.contacts !== null ? (
-          <ContactList
-            contacts={
-              this.state.filter[0] == null
-                ? this.state.contacts
-                : this.state.filter
-            }
-            deleteMe={this.deleteContacts}
-          />
-        ) : (
-          <div>We don't have any contact yet!</div>
-        )}
+        <ContactList
+          contacts={
+            this.state.filter[0] == null
+              ? this.state.contacts
+              : this.state.filter
+          }
+          deleteMe={this.deleteContacts}
+        />
       </>
     );
   }
--- src/components/ContactList/ContactList.jsx
+++ src/components/ContactList/ContactList.jsx
@@ -2,7 +2,7 @@ import css from './ContactList.module.css';
 import PropTypes from 'prop-types';
 
 export const ContactList = ({ contacts, deleteMe }) => {
-  return (
+  return contacts.length > 0 ? (
     <ul className={css.contactList}>
       {contacts.map(contact => (
         <li key={contact.id} className={css.contact}>
@@ -18,6 +18,8 @@ export const ContactList = ({ contacts, deleteMe }) => {
         </li>
       ))}
     </ul>
+  ) : (
+    <div className={css.contactList}>We don't have any contact yet!</div>
   );
 };
 
