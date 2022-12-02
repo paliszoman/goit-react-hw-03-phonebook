9524cda1b4858e41f5cc57d5fa29d387891aa046
paliszoman <kamilkrolik@hotmail.com>
2022-12-02 23:33:53 +0100

    new

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,86 +1,95 @@
-import React, { Component } from "react";
+import React, { Component } from 'react';
 
-import { Form } from "./Form/Form.jsx";
-import { Filter } from "./Filter/Filter.jsx";
-import { ContactList } from "./ContactList/ContactList.jsx";
-
-
+import { Form } from './Form/Form.jsx';
+import { Filter } from './Filter/Filter.jsx';
+import { ContactList } from './ContactList/ContactList.jsx';
 
 export class App extends Component {
-  
   static defaultProps = {
     contacts: [],
-  filter: ''
-  }
+    filter: '',
+  };
 
   constructor(props) {
-    super(props)
-    this.searchForContacts = this.searchForContacts.bind(this)
-    this.searchForExistingContacts = this.searchForExistingContacts.bind(this)
-    this.deleteContacts = this.deleteContacts.bind(this)
-    
+    super(props);
+    this.searchForContacts = this.searchForContacts.bind(this);
+    this.searchForExistingContacts = this.searchForExistingContacts.bind(this);
+    this.deleteContacts = this.deleteContacts.bind(this);
   }
 
   componentDidMount() {
-        if (this.state.contacts.length<=0) {
-      this.setState({contacts: JSON.parse(localStorage.getItem('phonebook'))})
-  }
+    if (this.state.contacts.length <= 0) {
+      this.setState({
+        contacts: JSON.parse(localStorage.getItem('phonebook')),
+      });
+    }
   }
 
   componentDidUpdate() {
-  
-  if (this.state !== localStorage.getItem('phonebook')) {
-    localStorage.setItem('phonebook', JSON.stringify(this.state.contacts))
+    if (this.state !== localStorage.getItem('phonebook')) {
+      localStorage.setItem('phonebook', JSON.stringify(this.state.contacts));
+    }
   }
-
-}
 
   //function for searching contacts by single letter
   searchForContacts(search) {
     const contacts = this.state.contacts;
     const filter = search.contact.toLowerCase();
-    let filterArray=[];
+    let filterArray = [];
     contacts.map(contact => {
       const hasName = contact.name.toLowerCase().includes(filter);
-      if(hasName) {filterArray.push(contact)}
-      return this.setState({filter: filterArray})
-    }
-    )
+      if (hasName) {
+        filterArray.push(contact);
+      }
+      return this.setState({ filter: filterArray });
+    });
   }
 
-
-//function looking for contact if exist make alert
+  //function looking for contact if exist make alert
   searchForExistingContacts(contactFromForm) {
     const contacts = this.state.contacts;
     const filter = contactFromForm.name.toLowerCase();
-    let contactsArray = contacts.find(contact =>contact.name.toLocaleLowerCase() === filter)
-  
-    return contactsArray===undefined ? this.setState({contacts:[...this.state.contacts,contactFromForm]}):alert('We have contact like this!')
+    let contactsArray = contacts.find(
+      contact => contact.name.toLocaleLowerCase() === filter
+    );
+
+    return contactsArray === undefined
+      ? this.setState({ contacts: [...this.state.contacts, contactFromForm] })
+      : alert('We have contact like this!');
   }
 
   //function that delete object with name from state
 
   deleteContacts(deleteContact) {
     const contacts = this.state.contacts;
-    let contactsArray = [...contacts]
-    let searchedContact = contacts.findIndex(contact => contact.name === deleteContact)
-    contactsArray.splice(searchedContact, 1)
-    return this.setState({contacts:contactsArray})
+    let contactsArray = [...contacts];
+    let searchedContact = contacts.findIndex(
+      contact => contact.name === deleteContact
+    );
+    contactsArray.splice(searchedContact, 1);
+    return this.setState({ contacts: contactsArray });
   }
 
   state = {
-  contacts: [],
-  filter:[]
-  }
+    contacts: [],
+    filter: [],
+  };
 
   render() {
     return (
       <>
         <h1>Phonebook</h1>
-        <Form onSubmit={value=>this.searchForExistingContacts(value)}></Form> 
+        <Form onSubmit={value => this.searchForExistingContacts(value)}></Form>
         <h2>Contacts</h2>
-        <Filter onChange={search=>this.searchForContacts(search)} ></Filter> 
-        <ContactList contacts={this.state.filter[0]==null?this.state.contacts:this.state.filter} deleteMe={this.deleteContacts}></ContactList>
+        <Filter onChange={search => this.searchForContacts(search)}></Filter>
+        <ContactList
+          contacts={
+            this.state.filter[0] == null
+              ? this.state.contacts
+              : this.state.filter
+          }
+          deleteMe={this.deleteContacts}
+        ></ContactList>
       </>
     );
   }
--- src/components/ContactList/ContactList.jsx
+++ src/components/ContactList/ContactList.jsx
@@ -1,21 +1,27 @@
 import css from './ContactList.module.css';
 import PropTypes from 'prop-types';
 
-
-
-
-export const ContactList = ({ contacts,deleteMe}) => {
-
-    return (
-        <ul className={css.contactList}>
-            {contacts.map(contact => <li key={contact.id} className={css.contact}>{contact.name}: {contact.number} <button type="submit" onClick={() => {deleteMe(contact.name)}}>x</button></li>)
-}
-        </ul>
-    )
-}
+export const ContactList = ({ contacts, deleteMe }) => {
+  return (
+    <ul className={css.contactList}>
+      {contacts.map(contact => (
+        <li key={contact.id} className={css.contact}>
+          {contact.name}: {contact.number}{' '}
+          <button
+            type="submit"
+            onClick={() => {
+              deleteMe(contact.name);
+            }}
+          >
+            x
+          </button>
+        </li>
+      ))}
+    </ul>
+  );
+};
 
 ContactList.propTypes = {
-    contacts: PropTypes.arrayOf(
-        PropTypes.objectOf(PropTypes.string)),
-    deleteMe: PropTypes.func
-}
+  contacts: PropTypes.arrayOf(PropTypes.objectOf(PropTypes.string)),
+  deleteMe: PropTypes.func,
+};
